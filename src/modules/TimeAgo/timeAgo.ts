2b7a8cac28175f4a127be63c13e87c253e859720
Wenderson Pires <wendersonpdas@gmail.com>
2024-04-26 12:46:44 -0300

    Update timeAgo.ts

--- src/modules/TimeAgo/timeAgo.ts
+++ src/modules/TimeAgo/timeAgo.ts
@@ -1,16 +1,25 @@
 import { UseModuleProps } from "alem";
 import en from "./en";
 
+// TODO: Fazer video tutorial disso
+// Code builder for "formatTimestamp"
+const formatTimestampCode = (timestamp: number) => {
+  const code = `
+    (() => {
+      TimeAgo.addDefaultLocale({
+        ${en}
+      });
+
+      return new TimeAgo('en-US').format(new Date(${timestamp}));
+    })()
+`;
+  return code;
+};
+
 const timeAgo = {
-  moduleSetup: `
-  TimeAgo.addDefaultLocale({
-    ${en}
-  })
-  `,
   formatTimestamp: (timestamp: number, onComplete: (data: string) => void) =>
     ({
-      setupCode: timeAgo.moduleSetup,
-      code: `new TimeAgo('en-US').format(new Date(${timestamp}))`,
+      code: formatTimestampCode(timestamp),
       onComplete,
     } as UseModuleProps),
 };
